d5c4f12ba3d65dda44394889cf3b1632c7157db1
Thiago <thiago.kamada@gmail.com>
2021-01-20 15:54:19 +0900

    Including DataSource

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const { ApolloServer } = require('apollo-server');
-const userSchema = require('./user/schema/user.graphql')
-const userResolvers = require('./user/resolvers/userResolvers')
+const userSchema = require('../api-apollo/api/user/schema/user.graphql')
+const userResolvers = require('../api-apollo/api/user/resolvers/userResolvers')
+const UsersAPI = require('../api-apollo/api/user/datasource/user')
 
 const typeDefs = [userSchema];
 const resolvers = [userResolvers];
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, dataSources: () =>{
+    return {
+        usersAPI: new UsersAPI()
+    }
+} });
 
 server.listen().then(({url}) =>{
     console.log(`Servidor rodando na porta ${url}`)
